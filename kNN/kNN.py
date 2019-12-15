64d1c52971f3cc9b597a99223ead8e22c689050d
kyle-cloud <1139467635@qq.com>
2019-12-15 16:00:18 +0800

    添加了读取文件和画图功能

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -1,4 +1,6 @@
 from numpy import*
+from matplotlib import*
+import matplotlib.pyplot as plt
 import operator
 
 def createDataSet():
@@ -22,3 +24,24 @@ def classify0(inX, dataSet, labels, k):
 
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
+def file2Matrix(filename):
+    fr = open(filename)
+    arrayLines = fr.readlines()
+    numberOfLines = len(arrayLines)
+    returnMat = zeros((numberOfLines, 3))
+    index = 0
+    classLabelVector = []
+    for line in arrayLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+def plotMap(datingDataMat):
+    fig = plt.figure()
+    ax =  fig.add_subplot(111)
+    ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2])
+    plt.show()
