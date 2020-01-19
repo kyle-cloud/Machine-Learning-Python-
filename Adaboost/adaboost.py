0a691a5b6920bfe383e63e8cd9ee7db7ac72c6a1
kyle-cloud <1139467635@qq.com>
2020-01-19 12:07:04 +0800

    简单数据加载

---
+++ Adaboost/adaboost.py
@@ -0,0 +1,10 @@
+def loadSimData():
+    dataMat = matrix([
+        [1., 2.1],
+        [2., 1.1],
+        [1.3, 1.],
+        [1., 1.],
+        [2., 1.]
+    ])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+    return dataMat, classLabels
