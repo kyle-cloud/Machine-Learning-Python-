40d3136cc29c0d307dfcf281f8e7e116716783f5
kyle-cloud <1139467635@qq.com>
2020-01-28 22:27:59 +0800

    魔鬼学习计划

--- Adaboost/adaboost.py
+++ Adaboost/adaboost.py
@@ -16,5 +16,6 @@ def stumpClassify(dataMatrix, demen, threshVal, threshIneq):
     今天太der了\
         移动要好好学英语啊
         明天开始以前的工作作息
+        明天大姐姐夫还有大姨回去了，嗯，今天实在不想写。不过明天就正式开始魔鬼学习了
 def buildStump(dataArr, classLabells, D):
 
