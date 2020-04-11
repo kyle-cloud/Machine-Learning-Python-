4a3b2be9a1d0eab0bd9bec567ef66fed6753efc5
kyle-cloud <1139467635@qq.com>
2020-04-11 23:23:20 +0800

    学习完成了adaboost，对测试集进行了试验

--- Adaboost/adaboost.py
+++ Adaboost/adaboost.py
@@ -77,3 +77,16 @@ def adaClassify(dataToClass, classifierArr):
         aggClassEst += classifierArr[i]['alpha'] * classEst
         print(aggClassEst)
     return sign(aggClassEst)
+
+def loadDataSet(filename):
+    numFeat = len(open(filename).readline().split('\t'))
+    dataMat = []; labelMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat - 1):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat, labelMat
